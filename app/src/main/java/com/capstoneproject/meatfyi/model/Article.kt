da0ab404bd9cdd6b6c7f70fce8f7ae4a24fa0531
aviano88 <67731935+aviano88@users.noreply.github.com>
2023-06-10 17:44:33 +0700

    Remove all files from main branch

--- app/src/main/java/com/capstoneproject/meatfyi/model/Article.kt
+++
@@ -1,8 +0,0 @@
-package com.capstoneproject.meatfyi.model
-
-data class Article(
-  val gambar : Int,
-  val judul : String,
-  val ringkasan : String,
-  val link : String
-);
--- app/src/main/java/com/capstoneproject/meatfyi/sharepreference/constant.kt
+++
@@ -1,9 +0,0 @@
-package com.capstoneproject.meatfyi.sharepreference
-
-class constant {
-    companion object{
-        val PREF_IS_LOGIN = "PREF_IS_LOGIN"
-        val PREF_EMAIL = "PREF_EMAIL"
-        val PREF_PASSWORD = "PREF_PASSWORD"
-    }
-}
